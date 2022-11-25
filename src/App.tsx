4d4361d78724cc6a65570a21c024200a2ab85fe1
Willian <willian.alves.b15@gmail.com>
2022-11-25 20:47:30 -0300

    feat: criando page Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <p>Willian</p>
+    <div>
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return(
+    <div>
+      <h1>Home</h1>
+      <p>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </p>
+    </div>
+  );
+}
