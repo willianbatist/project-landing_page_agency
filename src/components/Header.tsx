356382243e4e5a6e1606ec460cb27f822d73605c
Willian <willian.alves.b15@gmail.com>
2022-11-25 20:54:04 -0300

    feat: criando Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return(
+    <header>
+      <h1>Home</h1>
+      <p>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </p>
+    </header>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,9 @@
+import Header from '../components/Header';
+
 export default function Home() {
   return(
     <div>
-      <h1>Home</h1>
-      <p>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </p>
+      <Header/>
     </div>
   );
 }
