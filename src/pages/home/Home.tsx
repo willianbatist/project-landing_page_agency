a8c82e18283c5bb127a1e5972f9c5ae4e1822b30
Willian <willian.alves.b15@gmail.com>
2022-11-29 16:05:06 -0300

    feat: adicionando foto a home mais frases

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
 import Header from "../../components/header/Header";
 import { HomeContainer } from "./home.style";
+import imgOffice from '../../img/marketing-office.jpg'
 
 export default function Home() {
   return (
     <HomeContainer>
       <Header />
+      <img alt="Pessoas fazendo reunião numa mesa" src={ imgOffice } />
+      <h1>Digital Product Agency.</h1>
+      <p>Look no further. You came to the right ... place</p>
     </HomeContainer>
   );
 }
