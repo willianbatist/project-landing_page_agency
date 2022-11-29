e12b0dc4731f2fda1eb0c0a2ccf6bd7ed6d7534d
Willian <willian.alves.b15@gmail.com>
2022-11-29 15:49:02 -0300

    fix: lint

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import * as S from "./header.style";
 import { MenuMobile } from "../menuMobile/MenuMobile";
-import { RiMenu3Fill } from 'react-icons/ri';
+import { RiMenu3Fill } from "react-icons/ri";
 
 export default function Header() {
   const [menuIsVisible, setMenuIsVisible] = useState(false);
@@ -18,25 +18,40 @@ export default function Header() {
         </p>
         <ul className="nav-list">
           <li>
-            <a className="a" href="/">HOME</a>
+            <a className="a" href="/">
+              HOME
+            </a>
           </li>
           <li>
-            <a className="a" href="a">PRODUCTS</a>
+            <a className="a" href="a">
+              PRODUCTS
+            </a>
           </li>
           <li>
-            <a className="a" href="a">SERVICE</a>
+            <a className="a" href="a">
+              SERVICE
+            </a>
           </li>
           <li>
-            <a className="a" href="a">WORK</a>
+            <a className="a" href="a">
+              WORK
+            </a>
           </li>
           <li>
-            <a className="a" href="a">PRICING</a>
+            <a className="a" href="a">
+              PRICING
+            </a>
           </li>
           <li>
-            <a  className="a" href="a">CONTACT</a>
+            <a className="a" href="a">
+              CONTACT
+            </a>
           </li>
         </ul>
-        <RiMenu3Fill onClick={ () =>  setMenuIsVisible(true)} className="mobile"/>
+        <RiMenu3Fill
+          onClick={() => setMenuIsVisible(true)}
+          className="mobile"
+        />
       </S.NavList>
     </S.HeaderMenu>
   );
--- src/components/menuMobile/MenuMobile.tsx
+++ src/components/menuMobile/MenuMobile.tsx
@@ -7,10 +7,9 @@ interface Props {
 }
 
 export function MenuMobile({ menuIsVisible, setMenuIsVisible }: Props) {
-
-  return(
+  return (
     <Container isVisible={menuIsVisible}>
-      <IoClose size={45} onClick={() => setMenuIsVisible(false)}/>
+      <IoClose size={45} onClick={() => setMenuIsVisible(false)} />
       <nav>
         <a href="/">HOME</a>
         <a href="a">PRODUCTS</a>
@@ -20,5 +19,5 @@ export function MenuMobile({ menuIsVisible, setMenuIsVisible }: Props) {
         <a href="a">CONTACT</a>
       </nav>
     </Container>
-  )
+  );
 }
