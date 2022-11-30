7ecf97d6df5033962a7b2ebdf935abf9f652f2ee
Willian <willian.alves.b15@gmail.com>
2022-11-29 22:50:42 -0300

    feat: criando apiProducts

---
+++ src/services/services.ts
@@ -0,0 +1,28 @@
+import example1 from "../img/example1.jpg";
+import example2 from "../img/example2.jpg";
+import example3 from "../img/example3.jpg";
+
+export const apiProducts = [
+  {
+    id: 1,
+    img: example1,
+    name: "Workspace",
+    tag: "Management",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor",
+  },
+  {
+    id: 2,
+    img: example2,
+    name: "Minimalism",
+    tag: "Developmeny",
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor",
+  },
+  {
+    id: 3,
+    img: example3,
+    name: "Cleant Style",
+    tag: "Design",
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor",
+  },
+];
