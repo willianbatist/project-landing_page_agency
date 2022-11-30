dbf15966fed18eeca71bd542e3f15514737b577b
Willian <willian.alves.b15@gmail.com>
2022-11-30 19:09:27 -0300

    feat: criando Products

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,4 +1,6 @@
 import * as S from "./products.styles";
+import { apiProducts } from "../../services/services";
+import ProductCard from "./productCard";
 
 export default function Products() {
   return (
@@ -8,6 +10,15 @@ export default function Products() {
         Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor
         incididunt ut laboret dolore magna aliqua enim minim veniam exercitation
       </p>
+      { apiProducts.map((e) => (
+        <ProductCard
+          id={e.id}
+          img={e.img}
+          name={e.name}
+          tag={e.tag}
+          description={e.description}
+          />
+      )) }
     </S.Products>
   );
 }
