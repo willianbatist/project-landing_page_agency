f641f226061be458b7cf4e9d32cee13baa9cef9e
Willian <willian.alves.b15@gmail.com>
2022-11-30 19:07:42 -0300

    feat: criando productCard

---
+++ src/components/products/productCard.tsx
@@ -0,0 +1,25 @@
+import * as S from "./products.styles";
+
+interface Props {
+    id: number,
+    img: string,
+    name: string,
+    tag: string,
+    description: string,
+}
+
+export default function ProductCard({ id, img, name, tag, description  }: Props) {
+  return (
+    <S.ProductCard key={id}>
+      <div>
+        <img alt={name} src={img}/>
+      </div>
+      <h4>
+        <a href="#">{ name }</a>
+        <span>{ tag }</span>
+      </h4>
+      <p>{description}</p>
+      <a href="#">READ MORE</a>
+    </S.ProductCard>
+  );
+}
