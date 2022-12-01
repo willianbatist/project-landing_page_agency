e682258fd76737d348d345dca4007ae09bcd10f9
Willian <willian.alves.b15@gmail.com>
2022-12-01 10:56:51 -0300

    feat: criando PriceCard

---
+++ src/components/pricing/PriceCard.tsx
@@ -0,0 +1,29 @@
+import * as S from "./pricing.styles";
+
+interface Props {
+  id: number;
+  name: string;
+  price: string;
+  description: string;
+  item1: string;
+  item2: string;
+  item3: string;
+}
+
+export default function PriceCard(Props: Props) {
+  return (
+    <S.PriceCard key={Props.id}>
+      <h3>
+        {`${Props.name} `}
+        <span> - $</span>
+        {` ${Props.price}`}
+      </h3>
+      <p>{Props.description}</p>
+      <ul>
+        <li>{Props.item1}</li>
+        <li>{Props.item2}</li>
+        <li>{Props.item3}</li>
+      </ul>
+    </S.PriceCard>
+  );
+}
