a5be7dbb71fef4205c8dd9eef6d22fb29064a7ff
Willian <willian.alves.b15@gmail.com>
2022-11-30 21:19:59 -0300

    feat: colocando duas fotos no meio da pagina

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,8 @@ import Header from "../../components/header/Header";
 import Products from "../../components/products/Products";
 import * as S from "./home.style";
 import imgOffice from "../../img/marketing-office.jpg";
+import desktop1 from "../../img/desktop1.jpg";
+import desktop2 from "../../img/desktop2.jpg";
 
 export default function Home() {
   return (
@@ -17,6 +19,14 @@ export default function Home() {
       <h1>Digital Product Agency.</h1>
       <p>Look no further. You came to the right ... place</p>
       <Products />
+      <S.Section>
+        <div>
+          <img alt="imagem de um computador ligado" src={desktop1}/>
+        </div>
+        <div>
+        <img alt="imagem de um computador ligado" src={desktop2}/>
+        </div>
+      </S.Section>
     </S.HomeContainer>
   );
 }
--- src/pages/home/home.style.ts
+++ src/pages/home/home.style.ts
@@ -2,3 +2,5 @@ import styled from "styled-components";
 
 export const HomeContainer = styled.div`
 `;
+
+export const Section = styled.section``;
