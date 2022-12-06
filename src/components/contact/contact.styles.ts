113398c97c16243eb4bbab7de031ff993213d4c8
Willian <willian.alves.b15@gmail.com>
2022-12-06 14:56:46 -0300

    feat: criando contact.styles mais os estilos de Contact

---
+++ src/components/contact/contact.styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Contact = styled.section`
+  padding: 10px;
+
+  hr {
+    width: 100%;
+  }
+  h3 {
+    color: #515769;
+    margin: 15px 0 65px 0;
+    font-size: 40px;
+    text-align: center;
+    font-weight: 500;
+    line-height: 1.2;
+  }
+
+  p span {
+    font-weight: bold;
+    color: #17bed2;
+    font-size: 16px;
+  }
+
+  .icon {
+    color: #515769;
+    line-height: 55px;
+    font-size: 30px;
+    padding: 10px;
+  }
+
+  .divContact {
+    display: flex;
+    justify-content: space-evenly
+  }
+
+  .divContact div {
+    text-align: center;
+  }
+`;
