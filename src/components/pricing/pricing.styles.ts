70ad0c2a850a254a4c3cae0128e13de408796bb6
Willian <willian.alves.b15@gmail.com>
2022-12-06 23:44:16 -0300

    fix: novos pricing.styles

--- src/components/pricing/pricing.styles.ts
+++ src/components/pricing/pricing.styles.ts
@@ -1,5 +1,83 @@
 import styled from "styled-components";
 
-export const Pricing = styled.div``;
+export const Pricing = styled.div`
+  background-color: #fafafa;
+  height: 800px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 
-export const PriceCard = styled.div``;
+  .divApiPricing {
+    background-color: #fafafa;
+    display: flex;
+    max-width: 900px;
+    height: 90%;
+    align-items: center
+  }
+`;
+
+export const PriceCard = styled.div`
+  border: 2px solid black;
+  background-color: white;
+  max-width: 700px;
+  height: 50%;
+  margin: 80px 10px 60px 10px;
+  padding: 70px 45px;
+
+  h3 {
+    font-size: 22px;
+    font-weight: 700;
+    color: #515769;
+    line-height: 1.4;
+    margin: 0 0 15px;
+  }
+
+  h3 span {
+    font-size: 14px;
+    font-weight: 400;
+    color: #bfc1c7;
+  }
+
+  p {
+    font-size: 15px;
+    font-weight: 400;
+    color: #a6a7aa;
+    margin-top: 25px;
+    margin-bottom: 15px;
+  }
+
+  ul {
+    margin-top: 25px;
+    margin-bottom: 50px;
+    padding-left: 0;
+    list-style: none;
+  }
+
+  ul li {
+    position: relative;
+    font-size: 13px;
+    color: #81848f;
+    padding-left: 20px;
+    margin-bottom: 10px;
+    font-weight: 500;
+  }
+
+  a {
+    font-size: 13px;
+    font-weight: 600;
+    padding: 15px 40px;
+    color: #515769;
+    background: #f3f4f5;
+    border-color: transparent;
+    border-width: 0;
+  }
+
+  a:hover {
+    cursor: pointer;
+    background-color: #17bed2;
+    color: white;
+    transition-duration: .4s;
+    transition-property: all;
+    transition-timing-function: cubic-bezier(.7,1,.7,1);
+  }
+`;
